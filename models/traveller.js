f51381a302b6bd12c8a12ff2f9f6e7eb5b439e0d
Alexandria Rocks <alexandria.m.rocks@gmail.com>
2020-04-15 18:36:59 +0100

    completed hw

---
+++ models/traveller.js
@@ -0,0 +1,44 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+  })
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  })
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  });
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => {
+      return journey.distance > minDistance
+    });
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0);
+  return result;
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  const allJourneys = this.journeys;
+  const modesOfTransport = allJourneys.map(journey => journey.transport);
+  const uniqueModesOfTransport = modesOfTransport.filter((value, index, self) => self.indexOf(value) === index);
+  return uniqueModesOfTransport;
+};
+
+
+module.exports = Traveller;
